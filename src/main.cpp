e01a1a22a70837bc817b704c59b272791a118e4a
Roni Juppi <roni.juppi@gmail.com>
2018-06-15 02:00:57 +0300

    Fixed whitespace

--- src/main.cpp
+++ src/main.cpp
@@ -28,20 +28,20 @@ WindowProc(HWND hwnd, u32 message, WPARAM w_param, LPARAM l_param) {
     char* greeting = "Hello, World!";
     switch (message) {
         case WM_PAINT:
-            hdc = BeginPaint(hwnd, &ps);
+        hdc = BeginPaint(hwnd, &ps);
 
-            // Here your application is laid out.
-            // For this introduction, we just print out "Hello, World!"
-            // in the top left corner.
-            TextOutA(hdc, 5, 5, greeting, strlen(greeting));
-            // End application-specific layout section.
+        // Here your application is laid out.
+        // For this introduction, we just print out "Hello, World!"
+        // in the top left corner.
+        TextOutA(hdc, 5, 5, greeting, strlen(greeting));
+        // End application-specific layout section.
 
-            EndPaint(hwnd, &ps);
-            break;
+        EndPaint(hwnd, &ps);
+        break;
         //case WM_DESTROY:
         // TODO: We might want to do something if our window is destroyed:
         default:
-            return DefWindowProc(hwnd, message, w_param, l_param);
+        return DefWindowProc(hwnd, message, w_param, l_param);
     }
 
     return 0;

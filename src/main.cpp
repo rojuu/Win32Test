7b1517276b5e4c848f9679cba7a1be9a7acd97b4
Roni Juppi <roni.juppi@gmail.com>
2018-06-15 01:44:51 +0300

    Some cleanup and robustness

--- src/main.cpp
+++ src/main.cpp
@@ -20,13 +20,12 @@ typedef i16 b16;
 typedef i32 b32;
 typedef i64 b64;
 
-b32 RUNNING = true;
-
+#include <stdio.h>
 LRESULT
 WindowProc(HWND hwnd, u32 message, WPARAM w_param, LPARAM l_param) {
     PAINTSTRUCT ps;
     HDC hdc;
-    char greeting[] = _T("Hello, World!");
+    char* greeting = "Hello, World!";
     switch (message) {
         case WM_PAINT:
             hdc = BeginPaint(hwnd, &ps);
@@ -34,17 +33,13 @@ WindowProc(HWND hwnd, u32 message, WPARAM w_param, LPARAM l_param) {
             // Here your application is laid out.
             // For this introduction, we just print out "Hello, World!"
             // in the top left corner.
-            TextOutA(hdc,
-                    5, 5,
-                    greeting, strlen(greeting));
+            TextOutA(hdc, 5, 5, greeting, strlen(greeting));
             // End application-specific layout section.
 
             EndPaint(hwnd, &ps);
             break;
-        case WM_DESTROY:
-            RUNNING = false;
-            PostQuitMessage(0);
-            break;
+        //case WM_DESTROY:
+        // TODO: We might want to do something if our window is destroyed:
         default:
             return DefWindowProc(hwnd, message, w_param, l_param);
     }
@@ -54,16 +49,11 @@ WindowProc(HWND hwnd, u32 message, WPARAM w_param, LPARAM l_param) {
 
 i32
 WinMain(HINSTANCE instance, HINSTANCE prev_instance, char* cmd_line, i32 cmd_show) {
-    WNDCLASS window_class;
-    window_class.style = CS_HREDRAW|CS_VREDRAW;
+    WNDCLASS window_class = {};
+    window_class.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
     window_class.lpfnWndProc = WindowProc;
-    window_class.cbClsExtra = 0;
-    window_class.cbWndExtra = 0;
     window_class.hInstance = instance;
-    window_class.hIcon = 0;
-    window_class.hCursor = 0;
     window_class.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    window_class.lpszMenuName = 0;
     window_class.lpszClassName = "TestWindowClass";
 
     if(!RegisterClass(&window_class)) {
@@ -71,16 +61,12 @@ WinMain(HINSTANCE instance, HINSTANCE prev_instance, char* cmd_line, i32 cmd_sho
         return -1;
     }
 
-    HWND hwnd = CreateWindow("TestWindowClass", // Class name
+    HWND hwnd = CreateWindow(window_class.lpszClassName, // Class name
                              "Test", // Window name
-                             WS_OVERLAPPED|WS_CAPTION|WS_BORDER|
-                                WS_SYSMENU|WS_MINIMIZEBOX,//|WS_MAXIMIZEBOX|, // Style params
-                             CW_USEDEFAULT, CW_USEDEFAULT, //0, 0, // Position
-                             512, 512, // Size
-                             NULL, // Parent
-                             NULL, // Menu
-                             instance, // Instance
-                             NULL); // Param
+                             WS_OVERLAPPEDWINDOW,
+                             CW_USEDEFAULT, CW_USEDEFAULT, 512, 512,
+                             NULL, NULL, instance, NULL);
+
     if(!hwnd) {
         assert(false);
         return -1;
@@ -89,11 +75,14 @@ WinMain(HINSTANCE instance, HINSTANCE prev_instance, char* cmd_line, i32 cmd_sho
     ShowWindow(hwnd, cmd_show);
     UpdateWindow(hwnd);
 
-    while(RUNNING) {
-        MSG msg;
-        while (GetMessage(&msg, NULL, 0, 0)) {
+    MSG msg;
+    for(;;) {
+        BOOL result = GetMessage(&msg, hwnd, 0, 0);
+        if(result > 0) {
             TranslateMessage(&msg);
             DispatchMessage(&msg);
+        } else {
+            break;
         }
     }
 
